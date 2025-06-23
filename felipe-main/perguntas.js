b7cc2c48ba7b0aa1de4d5a78b91fbea5e2fc1fe7
felipenrz <felipe.peregrino@escola.pr.gov.br>
2025-06-23 09:36:26 -0300

    Add files via upload

---
+++ felipe-main/perguntas.js
@@ -0,0 +1,25 @@
+document.addEventListener('DOMContentLoaded', () => {
+    criaCartao(
+        'Oceano', 
+        'Qual é o maior oceano do mundo?', 
+        '0ceano Pacifico´
+    );
+
+    criaCartao(
+        'Livros', 
+        'Quem criou "Dom Quixote', 
+        'Mguel de Cervantes.'
+    );
+
+    criaCartao(
+        'pais', 
+        'Qual é a capital a australia?', 
+        'Canberra'
+    );
+
+    criaCartao(
+        'Ciencia', 
+        'Em que ano o homem pisou na lua pela primeira vez?', 
+        '1969'
+    );
+});
